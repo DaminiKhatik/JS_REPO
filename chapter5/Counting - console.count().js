8fc5ebb01b58288d5320043d0da649b3194a1cb6
DaminiKhatik <khatikdamini25@gmail.com>
2023-02-22 00:10:54 +0530

    added chapter5

---
+++ chapter5/Counting - console.count().js
@@ -0,0 +1,8 @@
+var o1 = 1,
+ o2 = '2',
+ o3 = ""; console.count(o1); 
+ console.count(o2); 
+ console.count(o3);
+console.count(1);
+ console.count('2');
+  console.count('');
---
+++ chapter5/formattingconsoleoutput.js
@@ -0,0 +1,5 @@
+console.log('%s has %d points', 'Sam', 100);
+
+console.log('%cHello world!', 'color: blue; font-size: xx-large');
+
+
